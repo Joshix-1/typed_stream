72c018d73de6692c6d51f5bf98f78b51ed56e9d3
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2023-02-14 15:59:39 +0100

    Create calculator.py

---
+++ examples/calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""A simple unsafe calculator."""
+import sys
+
+from typing import Any
+
+from typed_stream import Stream
+
+
+def print_prompt(*args: Any) -> None:
+    """Print an input prompt to stderr."""
+    print("> ", end="", flush=True)
+
+
+def main() -> None:
+    """Query inputs."""
+    print_prompt()
+    Stream(sys.stdin).map(eval).peek(print).for_each(print_prompt)
+
+
+if __name__ == "__main__":
+    main()
