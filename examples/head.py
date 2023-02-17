d99c84d49c24d7055963dbb26cb0ca67caeef8f1
Joshix-1 <joshix.dev@mailbox.org>
2023-02-17 12:00:00 +0000

    add head.py

---
+++ examples/head.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""A simple head program."""
+import sys
+
+from typed_stream import BinaryFileStream, Stream
+
+
+def head(*args: str) -> None | str:
+    """Print the head."""
+    if not args:
+        return "No file given. To read from stdin use '-'"
+    if len(args) > 2:
+        return "More than one file given."
+
+    stream: Stream[bytes]
+    if args[0] == "-":
+        print(
+            "Reading from stdin. To read from a file '-' use './-'",
+            file=sys.stderr,
+        )
+        stream = Stream(sys.stdin.buffer)
+    else:
+        stream = BinaryFileStream(args[0], True)
+
+    count = int(args[1]) if len(args) == 2 else 10
+    stream.limit(count).for_each(sys.stdout.buffer.write)
+    return None
+
+
+if __name__ == "__main__":
+    sys.exit(head(*sys.argv[1:]))
