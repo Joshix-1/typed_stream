0ab783abeca8f20ed9f2bf1754961e16e7a03278
Joshix-1 <joshix.dev@mailbox.org>
2023-02-20 23:00:00 +0000

    improve requirements file

--- check.sh
+++ check.sh
@@ -11,7 +11,7 @@ if [ -d venv ]; then
   fi
 fi
 
-pip_install="python3 -m pip install --disable-pip-version-check --require-virtualenv --quiet"
+pip_install="python3 -m pip install -U --disable-pip-version-check --require-virtualenv --quiet"
 ${pip_install} "pip>=23.0" wheel; exit_code="$?"
 if [ "${exit_code}" -ne 0 ] && [ "${exit_code}" -ne 3 ]; then
   echo "Installing pip>=23.0 and wheel failed"
