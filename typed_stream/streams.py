576e6b121cc287f95643d7ca8549c820ba2c595c
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2023-03-17 12:32:08 +0100

    Update streams.py

--- typed_stream/streams.py
+++ typed_stream/streams.py
@@ -768,10 +768,11 @@ class Stream(StreamABC[T], Iterable[T]):
             "Stream[tuple[T, U, V]]",
             "Stream[tuple[T, U]]",
             "Stream[IndexValueTuple[U]]",
-            "Stream[tuple[T]]]",
+            "Stream[tuple[T]]",
         ],
         fun: Callable[[T], K]
         | Callable[[T, U], K]
+        | Callable[[int, U], K]
         | Callable[[T, U, V], K]
         | Callable[[T, U, V, W], K]
         | Callable[[T, U, V, W, X], K],
