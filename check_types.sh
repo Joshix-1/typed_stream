c6cd4d9b0bf121d3444b994b89224196974ddfdb
Joshix <joshix.dev@mailbox.org>
2024-03-16 22:00:00 +0000

    remove pytype

--- check_types.sh
+++ check_types.sh
@@ -7,8 +7,8 @@ python3 -m mypy || FAILED="${FAILED}mypy "
 # echo stricter mypy:
 # python3 -m mypy --disallow-any-expr -p typed_stream || FAILED="${FAILED}mypy "
 
-echo pytype:
-python3 -m pytype || FAILED="${FAILED}pytype "
+# echo pytype:
+# python3 -m pytype || FAILED="${FAILED}pytype "
 
 # echo pyright:
 # python3 -m pyright || FAILED="${FAILED}pyright "
