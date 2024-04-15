b3c1ad1fa8a9ba8a83711236a83f50f88e8508f2
Joshix <joshix.dev@mailbox.org>
2024-04-15 18:00:00 +0000

    test nth_prime.sh

--- examples/test/__main__.py
+++ examples/test/__main__.py
@@ -2,8 +2,23 @@
 
 from __future__ import annotations
 
+from pathlib import Path
+from subprocess import run, PIPE
+
 from ..factoradic import test
 from ..print_primes import test_prime_stream
 
+EXAMPLES_DIR = Path(__file__).parent.parent
+
 test_prime_stream()
 test()
+
+
+def _get_nth_prime(num: int) -> int:
+    """Run nth_prime.sh."""
+    command = [(EXAMPLES_DIR / "nth_prime.sh").as_posix(), str(num)]
+    return int(run(command, check=True, stdout=PIPE).stdout)
+
+
+assert _get_nth_prime(100_000) == 1299709
+assert _get_nth_prime(200_000) == 2750159
