0cfce31a1ea306eb686d591f1b6a7c008e2f2cea
Joshix <joshix.dev@mailbox.org>
2024-04-24 19:00:00 +0000

    move doc-string

--- typed_stream/__init__.py
+++ typed_stream/__init__.py
@@ -2,7 +2,28 @@
 # You may obtain a copy of the licence in all the official languages of the
 # European Union at https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
 
-"""Typed Stream classes for easier handling of iterables."""
+"""Typed Stream classes for easier handling of iterables.
+
+Examples:
+>>> from typed_stream import FileStream, Stream
+>>> # Get sum of 10 squares
+>>> Stream.range(stop=10).map(lambda x: x * x).sum()
+285
+>>> # same as above
+>>> sum(Stream.counting().limit(10).map(pow, 2))
+285
+>>> # sum first 100 odd numbers
+>>> Stream.counting(start=1, step=2).limit(100).sum()
+10000
+>>> # Get the longest package name from requirements-dev.txt
+>>> (FileStream("requirements-dev.txt")
+...     .filter()
+...     .exclude(lambda line: line.startswith("#"))
+...     .map(str.split, "==")
+...     .starmap(lambda name, version = None: name)
+...     .max(key=len))
+'flake8-no-implicit-concat'
+"""
 
 from __future__ import annotations
 
